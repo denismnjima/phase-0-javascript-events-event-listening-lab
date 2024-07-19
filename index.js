0a3ec0e180fa82ea167ad4410d9753f7c1079485
denismnjima <denismnjima@gmail.com>
2024-07-19 16:46:23 +0300

    solved question

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 function addingEventListener() {
-}
+    const button = document.getElementById('button');
+    button.addEventListener('click', () => {
+      console.log('Button clicked');
+    });
+  }
+  const sinon = require('sinon');
+
+describe("index.js", () => {
+  let input;
+
+  beforeEach(function() {
+    input = document.getElementById('button');
+    sinon.spy(input, 'addEventListener');
+  })
+
+  afterEach(function() {
+    sinon.restore(); // Restore the original method after each test
+  });
+
+  it("binds an event listener in addingEventListener()", () => {
+    addingEventListener();
+    expect(input.addEventListener.called).to.be.true;
+    expect(input.addEventListener.calledWith('click')).to.be.true;
+  });
+});
+
